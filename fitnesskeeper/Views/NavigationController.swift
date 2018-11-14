2d2e3d35e204610b59996cd64fdf238993190ca3
Thaw zin oo <thaw.zin.oo12@gmail.com>
2018-11-14 11:44:19 +0630

    Code Group and Clean Code

--- fitnesskeeper/Views/NavigationController.swift
+++ fitnesskeeper/Views/NavigationController.swift
@@ -2,23 +2,23 @@
 //  NavigationController.swift
 //  LGSideMenuControllerDemo
 //
-import LGSideMenuController
-class NavigationController: UINavigationController {
-
-    override var shouldAutorotate : Bool {
-        return true
-    }
-    
-//    override var prefersStatusBarHidden : Bool {
-//        return UIInterfaceOrientation.isLandscape(UIApplication.shared.statusBarOrientation) && UI_USER_INTERFACE_IDIOM() == .phone
+//import LGSideMenuController
+//class NavigationController: UINavigationController {
+//
+//    override var shouldAutorotate : Bool {
+//        return true
 //    }
-    
-    override var preferredStatusBarStyle : UIStatusBarStyle {
-        return .default
-    }
-    
-    override var preferredStatusBarUpdateAnimation : UIStatusBarAnimation {
-        return sideMenuController!.isRightViewVisible ? .slide : .fade
-    }
-
-}
+//    
+////    override var prefersStatusBarHidden : Bool {
+////        return UIInterfaceOrientation.isLandscape(UIApplication.shared.statusBarOrientation) && UI_USER_INTERFACE_IDIOM() == .phone
+////    }
+//    
+//    override var preferredStatusBarStyle : UIStatusBarStyle {
+//        return .default
+//    }
+//    
+//    override var preferredStatusBarUpdateAnimation : UIStatusBarAnimation {
+//        return sideMenuController!.isRightViewVisible ? .slide : .fade
+//    }
+//
+//}
